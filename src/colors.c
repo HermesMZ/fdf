c4a7e982caa53d22db2bb387bea999c943bc3d54
Hermes <mathieuhermesfr@gmail.com>
2025-07-20 21:24:41 +0200

    switch colors

--- src/colors.c
+++ src/colors.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   colors.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mzimeris <mzimeris@student.42.fr>          +#+  +:+       +#+        */
+/*   By: zoum <zoum@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/16 19:33:15 by zoum              #+#    #+#             */
-/*   Updated: 2025/07/18 15:51:51 by mzimeris         ###   ########.fr       */
+/*   Updated: 2025/07/20 20:33:11 by zoum             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,4 +30,31 @@ int	interpolate_color(t_col c1, t_col c2, double ratio)
 	inter.b = (c1.b + (c2.b - c1.b) * ratio);
 	inter.t = (c1.t + (c2.t - c1.t) * ratio);
 	return (inter.t << 24 | inter.r << 16 | inter.g << 8 | inter.b);
+}
+
+
+unsigned int    *generate_gradient_palette(int z_min, int z_max)
+{
+	
+}
+
+void	update_colors(t_mlx_data *data)
+{
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	while (i < data->map->lines)
+	{
+		j = 0;
+		while (j < data->map->columns)
+		{
+			extract_rgb(&data->map->points[i][j].color,
+				get_gradient_color_for_z(data->map->points[i][j].z,
+					data->map->min_z, data->map->max_z));
+			j++;
+		}
+		i++;
+	}
 }
