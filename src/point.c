e05a62ff07f118d1f5bc7199f18c69fb02363da0
mzimeris <mzimeris@student.42.fr>
2025-07-21 17:01:57 +0200

    point.c norminette ok

--- src/point.c
+++ src/point.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   point.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: zoum <zoum@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: mzimeris <mzimeris@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/13 04:27:15 by zoum              #+#    #+#             */
-/*   Updated: 2025/07/20 19:10:37 by zoum             ###   ########.fr       */
+/*   Updated: 2025/07/21 17:01:12 by mzimeris         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,10 +44,9 @@ t_point	project(t_mlx_data *data, t_point p)
 	return (p);
 }
 
-void	link_points(t_mlx_data *data, int c, int l, t_point *point)
+static void	_column(t_mlx_data *data, int c, int l, t_point *point)
 {
 	t_point	point_right;
-	t_point	point_down;
 	t_point	point_tmp;
 
 	ft_memcpy(&point_tmp, point, sizeof(t_point));
@@ -61,6 +60,13 @@ void	link_points(t_mlx_data *data, int c, int l, t_point *point)
 		point_right = project(data, point_right);
 		draw_line(data, &point_tmp, &point_right);
 	}
+}
+
+static void	_line(t_mlx_data *data, int c, int l, t_point *point)
+{
+	t_point	point_down;
+	t_point	point_tmp;
+
 	ft_memcpy(&point_tmp, point, sizeof(t_point));
 	if (l < data->map->lines - 1)
 	{
@@ -73,3 +79,41 @@ void	link_points(t_mlx_data *data, int c, int l, t_point *point)
 		draw_line(data, &point_tmp, &point_down);
 	}
 }
+
+void	link_points(t_mlx_data *data, int c, int l, t_point *point)
+{
+	if (c < data->map->columns - 1)
+		_column(data, c, l, point);
+	if (l < data->map->lines - 1)
+		_line(data, c, l, point);
+}
+
+// void	link_points(t_mlx_data *data, int c, int l, t_point *point)
+// {
+// 	t_point	point_right;
+// 	t_point	point_down;
+// 	t_point	point_tmp;
+
+// 	ft_memcpy(&point_tmp, point, sizeof(t_point));
+// 	if (c < data->map->columns - 1)
+// 	{
+// 		point_right.x = (double)(c + 1);
+// 		point_right.y = (double)l;
+// 		point_right.z = (double)data->map->points[l][c + 1].z;
+// 		point_right.color = data->map->points[l][c + 1].color;
+// 		point_right = rotate(data, point_right);
+// 		point_right = project(data, point_right);
+// 		draw_line(data, &point_tmp, &point_right);
+// 	}
+// 	ft_memcpy(&point_tmp, point, sizeof(t_point));
+// 	if (l < data->map->lines - 1)
+// 	{
+// 		point_down.x = (double)c;
+// 		point_down.y = (double)(l + 1);
+// 		point_down.z = (double)data->map->points[l + 1][c].z;
+// 		point_down.color = data->map->points[l + 1][c].color;
+// 		point_down = rotate(data, point_down);
+// 		point_down = project(data, point_down);
+// 		draw_line(data, &point_tmp, &point_down);
+// 	}
+// }
